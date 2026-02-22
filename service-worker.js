5a3e143f70b1f60906ceae1b78590d7f6d637468
martadha3li <martadha3li@gmail.com>
2026-02-22 12:14:46 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('app-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js'
+      ]);
+    })
+  );
+});
